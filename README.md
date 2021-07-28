4f966dde69b75fb55586f16d2a789a554ec9ef62
Tristan Saldanha <tristansaldanha@gmail.com>
2021-07-28 15:07:27 -0700

    uploaded code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # 15puzzle
 Program to solve the (n*n)-1 puzzle
+
+I used A* search to solve this problem. It's similar to a simple BFS, but with an added "heuristic" mechanism so that 
+the distance of the "breadth" is defined by both A) how many moves you've made away from the starting position, and B) how far the result is to the solved state of the board. The hope here is that this will steer the search towards the solved state a little faster. 
+
+This algorithm makes sense for this problem because there's a very intuitive way to measure how far a given board is from the solved state. For each tile, sum up how far that tile is from where it needs to be. It's not a perfect metric, but it's enough to speed up the search. 
+
+As is, the program can solve the 2x2 and 3x3 case of the game quickly, but takes more than a few minutes to solve the 4x4. My bet is that the proximity to solved state function gets stuck in local minimas too easily. That would happen when many pieces are in their proper places, and then "trap" the last few pieces from moving to their spots. 
+
+### Features to improve performance:S
+- Make the h_dist heuristic more sophisticated to avoid local minimas. A good patch would be to assign a higher value to solving pieces around the edges first, preventing the "trapped" pieces issue. 
+- General python prettiness: there's definitely places where list comprehensions and things could improve code quality. Just me out of practice. 
+
+'''
---
+++ solver.py
@@ -0,0 +1,91 @@
+from scramble import (ImageScrambleGame, MOVE_UP,MOVE_LEFT,MOVE_DOWN,MOVE_RIGHT)
+import copy
+
+g = ImageScrambleGame(gsize=3)
+print("\nStart state: ", g.get_state(), "\n")
+
+# state to a line: should review list comprehensions
+def state_to_line(state):
+        res = []
+        for a in state:
+                for b in a:
+                        res.append(b)
+        return res
+
+#A* Heuristic: distance from any board state to the solved state
+# Sum of Manhattan distances of each tile to its solved position
+def h_dist(g):
+        manhattanDist = 0
+        t = (g.get_state())
+        t = state_to_line(t)
+        for c, tile in enumerate(t):
+                if tile == -1:
+                        continue
+                placedX, placedY = ((c) // g.gsize), ((c) % g.gsize)
+                desiredX, desiredY = ((tile) // g.gsize), ((tile) % g.gsize)
+                delt = abs(desiredX - placedX) + abs(desiredY - placedY)
+                manhattanDist += delt
+        return manhattanDist
+
+#get what moves are possible depending on where the empty spot is
+def legal_moves(g):
+        goods = []
+        p = g.whitespot_idx
+        if (p % g.gsize) != 0: goods.append(1)          #can move left
+        if p > g.gsize-1: goods.append(0)               #can move up
+        if ((p+1) % g.gsize) != 0: goods.append(3)      #can move right
+        if p < (g.gsize*(g.gsize-1))-1: goods.append(2) #can move down
+        return goods
+
+#node class to help bundle some things together for the A* search methods
+class node():
+        def __init__(self, g=None, history=[]):
+                self.g = g
+                self.history = history
+                self.distOut = len(history)
+                self.hDist = h_dist(g)
+                self.totalDist = self.distOut + self.hDist
+        def update(self):
+                self.distOut = len(self.history)
+                self.hDist = h_dist(g)
+                self.totalDist = self.distOut + h_dist(self.g)
+
+start = node(g)
+queue = [start]
+seen = set()
+seen.add(hash(str(start.g.get_state()))) 
+#A set prevents us from re-evaulating already seen states, such as making then un-making the same moves
+
+
+#dumb queue insertion
+#should definitely make this better for runtime's sake
+def inPoint(e):
+        for c,g in enumerate(queue):
+                if g.totalDist > e.totalDist:
+                        return c
+        return -1
+
+#main A*
+
+t = 1
+while queue:
+        if t % 1000 == 0:
+                print("tested", t, "solutions")
+        cur = queue[0]
+        queue.remove(cur)
+        dist = h_dist(cur.g)
+        if dist == 0:
+                print("Solution found!")
+                print("\n", cur.history)
+                break
+        steps = legal_moves(cur.g)
+        for s in steps:
+                new = copy.deepcopy(cur)
+                new.g = copy.deepcopy(cur.g) #this might be overkill but it definitely works this way 
+                new.g.move_blank(s)
+                new.update()
+                if hash(str(new.g.get_state())) not in seen:
+                        new.history.append(s)
+                        queue.insert(inPoint(new), new)
+                        t += 1
+                        seen.add(hash(str(new.g.get_state()))) 
